ce9fe4e4512c5cf1e552e01574de6349439f3778
Dmytro Pavlov <dmitriy.paulov@gmail.com>
2023-12-26 16:21:50 +0200

    [16.0][REF] stock_packed_picking: added missing docstrings

--- stock_packed_picking/models/stock_picking.py
+++ stock_packed_picking/models/stock_picking.py
@@ -13,6 +13,26 @@ class Picking(models.Model):
         location=None,
         location_dest_id=None,
     ):
+        """
+        Prepare values for stock.picking record.
+        Is a part of packed picking creation functionality.
+        See also '_create_packed_picking' method.
+
+        Args:
+        stock_move_data (List of tuples): [(product_id, qty_done, serial)]
+        - (Integer) product_id: id of the product
+        - (float) qty_done: quantity done
+        - (Char, optional) serial: serial number to assign
+        operation_type (stock.picking.type): Operation type
+        owner (res.partner, optional): Owner of the product
+        location (stock.location, optional): Source location if differs from the
+        operation type one
+        location_dest (stock.location, optional): Destination location if differs
+        from the operation type one
+
+        Returns:
+        dict: Prepared stock.picking values
+        """
         location = location or operation_type.default_location_src_id
         location_dest_id = location_dest_id or operation_type.default_location_dest_id
         vals = {
@@ -40,6 +60,18 @@ class Picking(models.Model):
         product_id,
         qty_done,
     ):
+        """
+        Prepare values for stock.move.line record.
+        Is a part of packed picking creation functionality.
+        See also '_create_packed_picking' method.
+
+        Args:
+        product_id (Integer): ID of product.product record.
+        qty_done (Float): Quantity done.
+
+        Returns:
+        dict: Prepared stock.move.line values
+        """
         return {
             "product_id": product_id,
             "qty_done": qty_done,
@@ -51,6 +83,19 @@ class Picking(models.Model):
         move_line,
         serial=None,
     ):
+        """
+        Prepare values for stock.lot record.
+        Is a part of packed picking creation functionality.
+        See also '_create_packed_picking' method.
+
+        Args:
+        move_line (Record: stock.move.line): Move line record
+        serial (Char, optional): Serial number to assign.
+        If empty, system will generate serial number automatically.
+
+        Returns:
+        dict: Prepared stock.lot values
+        """
         vals = {
             "product_id": move_line.product_id.id,
             "company_id": move_line.company_id.id,
